48099819a991a171db03ff72b583fffc52d72510
JeongHeumChoi <79458446+JeongHeumChoi@users.noreply.github.com>
2024-02-19 17:32:03 +0900

    WAKEUP DOCKER.. PLZ

--- src/main/java/sideproject/puddy/controller/ChatController.java
+++ src/main/java/sideproject/puddy/controller/ChatController.java
@@ -14,6 +14,7 @@ import sideproject.puddy.service.ChatService;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 public class ChatController {
+    
     private final ChatService chatService;
     private final ChatAndMessageService chatAndMessageService;
 
